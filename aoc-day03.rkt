a8249ab328feacae3d532f8578e49aef229527b4
Ronie Uliana <ronie.uliana@shopify.com>
2020-12-03 08:52:38 -0500

    Day 03 - second star
    What I like:
    - This one builds on top of the last one. Cool!
    
    What I don't like:
    - I could avoid some parameter massaging. Interestingly, some of the alternatives
    (like creating a struct) would actually generate the same amount of code. I boils
    down to the amount of reuse...

--- aoc-day03.rkt
+++ aoc-day03.rkt
@@ -19,7 +19,7 @@
              [1 0 0 0 1 1 0 0 0 0 1]
              [0 1 0 0 1 0 0 0 1 0 1]])))
 
-
+;; The main logic
 (define (tree-hits by-row by-col test-field)
   (define (at-pos r c) (matrix-ref test-field r c))
   (define num-rows (matrix-num-rows test-field))
@@ -35,7 +35,23 @@
 (module+ test
   (check-equal? (tree-hits 1 3 test-field) 7))
 
+;; Just massaging parameters :(
+(define ((field-trier field) coord)
+  (tree-hits (car coord) (cadr coord) field))
 
+(module+ test
+  (check-equal? ((field-trier test-field) '(1 3)) 7))
+
+;; Answers the second star (multiplication not here)
+(define (multi-field-tries field tries)
+  (map (field-trier field) tries))
+
+(module+ test
+  (check-equal? (multi-field-tries test-field '((1 1) (1 3) (1 5) (1 7) (2 1)))
+                '(2 7 3 4 2)))
+
+
+;; Deal with the input
 (define (.#->01 char)
   (if (equal? char #\.) 0 1))
 
@@ -44,8 +60,15 @@
        string->list
        (map .#->01)))
 
-(~>> "./aoc-day03.input"
-     file->lines
-     (map string->01-list)
-     list*->matrix
-     (tree-hits 1 3))
+;; Create inputs used in the problems
+(define field
+  (~>> "./aoc-day03.input"
+       file->lines
+       (map string->01-list)
+       list*->matrix))
+
+(define tries '((1 1) (1 3) (1 5) (1 7) (2 1)))
+
+;; The answers
+(printf "Day 03 - star 1: ~a\n" (tree-hits 1 3 field))
+(printf "Day 03 - star 2: ~a\n" (apply * (multi-field-tries field tries)))
