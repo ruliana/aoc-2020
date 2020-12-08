0ff4225ab082f757f4f5ccd67fc3f0261d4d18ea
Ronie Uliana <ronie.uliana@shopify.com>
2020-12-08 13:46:49 -0500

    Day 8 - refactoring before second star
    I couldn't resist and I've added some macro here.
    
    What's the point of doing the Advent of Code in Racket if I can't
    sprinkle some macros here and there, right? :D
    
    TBH, that's the most powerful feature of Racket, but also it's Achilles
    heel. You can do EVERYTHING, usually even better than the original
    languages which it's a main feature (check pattern matching vs Haskell
    version or threading macro vs Clojure version). On the other hand,
    Racket code from someone is different on level that the usual programmer
    are not used to.
    
    Like create a new form of defining functions as I did here.
    
    It's truly a language to create programming languages. Or better, a
    programming language oriented programming language :dizzy:.

--- aoc-day08.rkt
+++ aoc-day08.rkt
@@ -1,22 +1,34 @@
 #lang racket
 (require threading
-         racket/file)
+         racket/file
+         syntax/parse
+         (for-syntax racket/syntax))
 
 (module+ test
   (require rackunit))
 
 
-(define (run-program program-vector)
-  (let loop ([cursor 0]
-             [acc 0]
-             [tracer empty])
-    (define instruction (vector-ref program-vector cursor))
-    (define new-tracer (cons cursor tracer))
-    (if (member cursor tracer) acc
+;; A bit of macro magic to create a function with
+;; and early return point (called "break" here)
+(define-syntax (define/break stx)
+  (syntax-case stx ()
+    [(_ (name args ...) body ...)
+     (with-syntax ([break-id (format-id #'name "break")])
+        #'(define (name args ...)
+            (let/cc break-id body ...)))]))
+
+
+(define/break (run-program program-vector)
+    (let loop ([cursor 0]
+               [acc 0]
+               [tracer empty])
+        (define instruction (vector-ref program-vector cursor))
+        (define new-tracer (cons cursor tracer))
+        (when (member cursor tracer) (break acc))
         (match instruction
             [(cons 'nop _) (loop (add1 cursor) acc new-tracer)]
             [(cons 'acc n) (loop (add1 cursor) (+ n acc) new-tracer)]
-            [(cons 'jmp n) (loop (+ n cursor) acc new-tracer)]))))
+            [(cons 'jmp n) (loop (+ n cursor) acc new-tracer)])))
 
 (module+ test
   (define test-program
